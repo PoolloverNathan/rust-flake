f47108f4623087c84bd7e4c0fb22d77e248d8d0e
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-05-03 17:43:26 -0400

    flake mobile

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=9155c3d38dce4d0a718ea58fc4a73c8981041497";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
+    packages.aarch64-linux.default = import ./default.nix { pkgs = nixpkgs.legacyPackages.aarch64-linux; };
   };
 }
