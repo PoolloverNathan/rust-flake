902cd04cd171cef9bc4628c0f10593d32213ad48
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-05-03 15:54:11 -0400

    flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=9155c3d38dce4d0a718ea58fc4a73c8981041497";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.fia = import ./default.nix { pkgs = nixpkgs; };
+  };
+}
